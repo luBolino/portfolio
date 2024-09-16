1114cda036a1535f9f1c8dfd90353fc8b9f191b9
Luana Bolino <luana.bolino.dv@gmail.com>
2024-09-16 14:01:39 -0300

    Add files via upload

---
+++ assets/resources/scripts/main.js
@@ -0,0 +1,17 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.menu-icon').src="/assets/resources/img/svg/menu_white_36dp.svg"
+    } else{
+        menuMobile.classList.add('open');
+        document.querySelector('.menu-icon').src="/assets/resources/img/svg/close.svg";
+    }
+}
+
+document.getElementById("download-btn").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "./"; 
+    link.download = "curriculo.pdf"; 
+    link.click();
+  });
